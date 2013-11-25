d15e332691e9116b894e1fb3514cd8f755109f94
Jesse Mullan <jmullan@visi.com>
2013-11-25 14:54:43 -0800

    add working tests

---
+++ tests/ScorerTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace tests;
+
+class ScorerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPickLocale()
+    {
+        $scorer = new \AcceptToGettext\Scorer();
+        $result = $scorer->pickLocale(array('de_DE'), array());
+        $this->assertEquals($result, array ('de-de', 'UTF-8', 'de_DE'));
+
+        $result = $scorer->pickLocale(array('de_DE', 'cs_CZ.UTF-8'), array());
+        $this->assertEquals($result, array ('de-de', 'UTF-8', 'de_DE'));
+
+        $result = $scorer->pickLocale(array('de_DE', 'cs_CZ.UTF-8'), array('HTTP_ACCEPT_LANGUAGE' => 'cs'));
+        $this->assertEquals($result, array ('cs-cz', 'UTF-8', 'cs_CZ.UTF-8'));
+    }
+}
